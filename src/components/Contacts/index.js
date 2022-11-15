bfca04780690fe3cb900c82db199511f8d3048f7
Drew Speed <drewjspeed@gmail.com>
2022-11-14 21:38:34 -0600

    added form validation

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
-
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-    }
+    const [errorMessage, setErrorMessage] = useState('');
 
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+
+        }
+        setFormState({...formState, [e.target.name]: e.target.value })
+        console.log('errorMessage', errorMessage)
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+
+    }
 
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name: </label>
                     <input type="text" name="name" defaultValue={name} onChange={handleChange} />
